94e72d894883bcb4471941aa66e78f724bf9e6e7
azim2606 <azimhao@gmail.com>
2023-10-12 10:15:26 +0800

    remove some pages and add new pagesAdd project files.

---
+++ Shelf of Tales bookstore/Pages/Feedback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shelf_of_Tales_bookstore.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
