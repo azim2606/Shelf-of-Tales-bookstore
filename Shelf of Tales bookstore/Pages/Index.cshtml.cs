94150cc8d38c7c131d9cd6bebda0888689a8e887
azim2606 <azimhao@gmail.com>
2023-10-12 10:15:26 +0800

    Add project files.

---
+++ Shelf of Tales bookstore/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shelf_of_Tales_bookstore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
