ccb46405636cf504b24aa27d031265dde1c65ca5
azim2606 <azimhao@gmail.com>
2023-10-27 23:47:58 +0800

    update a little

--- Shelf of Tales bookstore/Models/Registration.cs
+++ Shelf of Tales bookstore/Models/Registration.cs
@@ -4,6 +4,9 @@ namespace Shelf_of_Tales_bookstore.Models
 {
     public class Registration
     {
+        
+        public string ID { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
